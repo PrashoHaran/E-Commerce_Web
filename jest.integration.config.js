552ecd007ef35e770197a19d4ad3b28f82697308
PrashoHaran <prashoharan8962@gmail.com>
2025-04-13 01:03:14 +0530

    Add jest unit and integration config files

---
+++ jest.integration.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testMatch: ["**/tests/integration/**/*.test.js"],
+    testEnvironment: "node",
+    verbose: true,
+    collectCoverage: true,
+    coverageDirectory: "coverage/integration",
+  };
+  
