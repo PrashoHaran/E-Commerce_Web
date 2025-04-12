ecdebd91b547870c4dc93df2600138ab510eb4f3
Prashoharan <162808455+PrashoHaran@users.noreply.github.com>
2025-04-13 00:24:01 +0530

    Delete tests directory

--- tests/integration/api.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../../app');  // Adjust path to your main app file
-
-describe('GET /', () => {
-  it('should return 200 OK', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
-  });
-});
